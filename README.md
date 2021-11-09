5a64e6c61d1de7003df7d3935a4de429ff733ded
charstnut <36014603+charstnut@users.noreply.github.com>
2021-11-09 11:04:59 -0500

    Updates structure

---
+++ README.md
@@ -0,0 +1,68 @@
+# Ray Tracing
+This folder contains a package for quick muon-detector simulations. 
+
+# Installation for development
+Add the current package to Julia general envronment for dev, it will be recorded in the general Manifest file:
+```julia
+]dev /path/to/package/
+```
+To remove (since this package is local and not registered under the official Julia package repo):
+```julia
+]rm packagename
+```
+
+Note: the required packages include `PyCall` which deals with python bindings.
+Its default python executable and environment can be set when installed. 
+I just bind it to my conda production environment which has `jupyter` installed.
+To bind a specific python environment, run the following commands in julia:
+```julia
+using Pkg;
+Pkg.activate()
+
+if Base.find_package("PyCall") === nothing
+    # Check if the production environment has been created
+    prod_env = "prod_linux" # <-- this is the environment where you want PyCall to bind to
+    file = "eval \"\$(conda shell.bash hook)\" \nconda activate $prod_env && which python"
+    file_name = joinpath(ENV["HOME"], ".julia/config/tmp.sh")
+    open(file_name, "w") do f
+        println(f, file)
+        flush(f)
+    end
+    if !success(Cmd(`bash $file_name`))
+        @warn "Conda $prod_env cannot be activated. Exit building PyCall..."
+    else
+        Pkg.add("PyCall")
+        pypath = ENV["PYTHON"] = readchomp(`bash $file_name`)
+        println("Using $pypath as python path.")
+        Pkg.build("PyCall")
+    end
+    rm(file_name)
+else
+```
+
+You would then have the `PyCall` package installed under your root environment.
+All subsequent python wrapper package would use the same python binding as 
+the `PyCall` package.
+
+Reference: https://github.com/JuliaPy/PyCall.jl
+
+# Usage
+
+To run a simulation, simply add the `CoincidenceSim` package to your Julia root environment (see above) and use it as a normal package.
+For example:
+```julia
+using CoincidenceSim
+sim_num = Int(1e3)
+r = 0.80
+ℓ = 0.75
+det1 = RectBox(0.01, 0.05, 0.05, position=(0, 0, 0), orientation=deg2rad.((0, 0)), efficiency=0.98, material="POP Doped Polystyrene")
+detectors = [det1]
+runhemisim(sim_num, detectors, r, (0, 0, 0), ℓ)
+```
+
+# TODOs:
+- [High Priority] Complete the tests.
+- [Medium Priority] Explore the interface with python.
+- [Low Priority] Construct a multip-dimensional sampler for arbitrary distributions
+    - Motivation: how to efficiently sample an arbitrary multivariate distribution given it's analytical form?
+    - We also needs to sample its integrated probability density, for example: sample the energy spectrum of a muon after we know its solid angle.
---
+++ src/SimUtils.jl
@@ -0,0 +1,124 @@
+### This script contains functions used for simulations
+
+using FLoops
+using SparseArrays
+using DataStructures
+
+
+"""
+Runs simulation given a list of detectors, and spawns the rays with 2max_x × 2max_x area @ center.
+Note that the detectors are a vector of types being the subtypes of the LabObject (general method).
+The sparse boolean array is stored with dimension (n_sim × n_detector).
+The information for each hit event is stored in dictionary for each detector.
+    """
+function runsim(n_sim::Int, detectors::Vector{T}, 
+    max_x::Real, center::NTuple{3,Real}; exec=ThreadedEx()) where {T <: LabObject{<:Real}}
+    @floop exec for i in 1:n_sim
+        # Private mutable variables
+        @init begin
+            crosses = SortedDict{Float64,SVector{3,Float64}}()
+            hit_vec = falses(length(detectors))
+            i_vec = zeros(length(detectors))
+            j_vec = zeros(length(detectors))
+            crx = Vector{Union{Missing,Dict}}(missing, length(detectors))
+            dir = Vector{Union{Missing,Dict}}(missing, length(detectors))
+        end
+        # Set the hit_vec to false to prepare for a new ray
+        hit_vec .*= false
+        ray = Ray((max_x, max_x), center)
+        # Loop through each dectector to fill the pre-allocated vectors
+        for (j, d) in enumerate(detectors)
+            hit = isthrough!(ray, d, crosses)
+            if hit
+                i_vec[j] = i
+                j_vec[j] = j
+                hit_vec[j] = hit
+                points = hcat(values(crosses)...)'
+                # Note the points are vertically concatenated
+                crx[j] = Dict(i => points)
+                dir[j] = Dict(i => Float64[ray.azimuth_agl, ray.polar_agl])
+                empty!(crosses)
+            end
+        end
+        # Hit indices to be filled
+        ii = i_vec[hit_vec]
+        jj = j_vec[hit_vec]
+        # Reduce the accumulators
+        # Note that for threaded executions, the initial values will be
+        # assigned to the variables (e.g. ii) at the end of the loop
+        # This means e.g. ii will be Int[] at some time
+        @reduce() do (I = Int[]; ii), 
+            (J = Int[]; jj), 
+            (crx_pts = [Dict{Int,Matrix{Float64}}() for i in 1:length(detectors)]; crx), 
+            (ray_dirs = [Dict{Int,Vector{Float64}}() for i in 1:length(detectors)]; dir)
+            append!(I, ii)
+            append!(J, jj)
+            for (j, c) in enumerate(crx)
+                if !ismissing(c) && !isempty(c)
+                    merge!(crx_pts[j], crx[j])
+                    merge!(ray_dirs[j], dir[j])
+                end
+            end
+        end
+    end
+    results = sparse(I, J, trues(length(I)), n_sim, length(detectors))
+    return results, crx_pts, ray_dirs
+end
+
+
+"""
+Runs the simulation a hemispherical generating surface. 
+    """
+function runhemisim(n_sim::Int, detectors::Vector{T}, 
+    R::Real, center::NTuple{3,Real}, ℓ::Real; exec=ThreadedEx()) where {T <: LabObject{<:Real}}
+    @floop exec for i in 1:n_sim
+        # Private mutable variables
+        @init begin
+            crosses = SortedDict{Float64,SVector{3,Float64}}()
+            hit_vec = falses(length(detectors))
+            i_vec = zeros(length(detectors))
+            j_vec = zeros(length(detectors))
+            crx = Vector{Union{Missing,Dict}}(missing, length(detectors))
+            dir = Vector{Union{Missing,Dict}}(missing, length(detectors))
+        end
+        # Set the hit_vec to false to prepare for a new ray
+        hit_vec .*= false
+        ray = Ray(R, center, ℓ)
+        # Loop through each dectector to fill the pre-allocated vectors
+        for (j, d) in enumerate(detectors)
+            hit = isthrough!(ray, d, crosses)
+            if hit
+                i_vec[j] = i
+                j_vec[j] = j
+                hit_vec[j] = hit
+                points = hcat(values(crosses)...)'
+                # Note the points are vertically concatenated
+                crx[j] = Dict(i => points)
+                dir[j] = Dict(i => Float64[ray.azimuth_agl, ray.polar_agl])
+                empty!(crosses)
+            end
+        end
+        # Hit indices to be filled
+        ii = i_vec[hit_vec]
+        jj = j_vec[hit_vec]
+        # Reduce the accumulators
+        # Note that for threaded executions, the initial values will be
+        # assigned to the variables (e.g. ii) at the end of the loop
+        # This means e.g. ii will be Int[] at some time
+        @reduce() do (I = Int[]; ii), 
+            (J = Int[]; jj), 
+            (crx_pts = [Dict{Int,Matrix{Float64}}() for i in 1:length(detectors)]; crx), 
+            (ray_dirs = [Dict{Int,Vector{Float64}}() for i in 1:length(detectors)]; dir)
+            append!(I, ii)
+            append!(J, jj)
+            for (j, c) in enumerate(crx)
+                if !ismissing(c) && !isempty(c)
+                    merge!(crx_pts[j], crx[j])
+                    merge!(ray_dirs[j], dir[j])
+                end
+            end
+        end
+    end
+    results = sparse(I, J, trues(length(I)), n_sim, length(detectors))
+    return results, crx_pts, ray_dirs
+end
