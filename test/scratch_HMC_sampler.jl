ef6bcc439c8f4f1d927a6d89e8fb25428c26d699
charstnut <36014603+charstnut@users.noreply.github.com>
2023-02-23 14:25:07 -0500

    Renames scratch

--- test/scratch_HMC_sampler.jl
+++ test/scratch_HMC_sampler.jl
@@ -1,4 +1,6 @@
+# %%
 using StatsBase, Distributions, Interpolations, Plots, LinearAlgebra, Random
+using LogDensityProblems, LogDensityProblemsAD, TransformVariables, TransformedLogDensities, DynamicHMC
 
 # %%
 # Creating original samples...
@@ -8,11 +10,6 @@ diag_V = [0.1, 0.5]
 N = MvNormal(mean_V, diag_V)
 x_range = 0:0.005:1
 y_range = -1:0.01:1
-
-
-# %%
-# Define the log likelihood function
-using LogDensityProblems, LogDensityProblemsAD, TransformVariables, TransformedLogDensities, DynamicHMC
 
 function (D::ContinuousUnivariateDistribution)(θ::NamedTuple)
     return logpdf(D, θ.x)
@@ -119,49 +116,3 @@ end
 histogram(res[2, :], label="θ", normalize=:pdf)
 plot!(θ_range, [θ_pdf(p, t) for t in θ_range], label="θ theoretical")
 
-# %%
-itp = linear_interpolation(edges, (eCDF(edges)), extrapolation_bc=Flat())
-
-# %%
-
-result = []
-for n in 1:10000
-    push!(result, unisamplercdf!(itp; low_bound=floor(minimum(eCDF)), upp_bound=ceil(maximum(eCDF))))
-end
-
-grad = []
-for e in edges
-    push!(grad, gradient(itp, e)[1])
-end
-
-plot(edges, grad)
-histogram!(obs, bins=100, normalize=:pdf, alpha=0.3)
-histogram!(result, bins=100, normalize=:pdf, alpha=0.3)
-
-# %%
-X = MCIntegration.Continuous(0.0, 2.0)
-res = integrate((X, c) -> (X[1]^2 + X[2]^2 < 1.5); var=X, dof=2)
-println(res.mean)
-plot(res.config.var[1].grid)
-
-# Cdf --> Pmf..
-# pmf_obs = Float64[]
-# for i in 1:length(obs)
-#     if i == 1
-#         append!(pmf_obs, cdf_obs[1])
-#     else
-#         append!(pmf_obs, cdf_obs[i] - cdf_obs[i-1])
-#     end
-# end
-# # Creating a full distribution object..
-# d = DiscreteNonParametric(obs, pmf_obs)
-# # Sampling from the distribution object..
-# out = rand(d, 100)
-# # Test it...
-# plot(sort(out))
-# # quantile(d, 0.5)
-# # quantile(d, 1 - 0.025)
-
-# Current problem:
-# 1. Interpolation of eCDF and sampling: interpolation is not monotonic and stable, and sampling also could not use CDF directly.
-# 2. MC integration does not output explicit optimized variables. Have a hard time to calculate derived quantities.
